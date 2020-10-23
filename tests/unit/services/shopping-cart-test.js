357bc448292ed185519a3c6385c6d7160145979b
Shawn Chen <ethanloatrchen@gmail.com>
2020-10-22 17:04:09 -0700

    Tutorial 11

--- tests/unit/services/shopping-cart-test.js
+++ tests/unit/services/shopping-cart-test.js
@@ -9,4 +9,11 @@ module('Unit | Service | shopping-cart', function(hooks) {
     let service = this.owner.lookup('service:shopping-cart');
     assert.ok(service);
   });
+
+  test('addItem works', function(assert) {
+    const service = this.owner.lookup('service:shopping-cart');
+    assert.equal(service.itemList.length, 0, 'item list is empty');
+    service.addItem({});
+    assert.equal(service.itemList.length, 1, 'item list size increase by 1');
+  })
 });
