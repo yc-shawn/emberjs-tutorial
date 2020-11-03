8ed3f297c3a40b70d442b6f1a9656b3178ce326b
Shawn Chen <ethanloatrchen@gmail.com>
2020-11-02 16:49:58 -0800

    Tutorial 12

--- tests/integration/components/product/details-test.js
+++ tests/integration/components/product/details-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | product/details', function(hooks) {
@@ -9,18 +9,25 @@ module('Integration | Component | product/details', function(hooks) {
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Product::Details />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Product::Details>
-        template block text
-      </Product::Details>
+    this.set('price', {
+      original: 50,
+      current: 30,
+    })
+    this.set('onChangeColor', function(color) {
+      assert.equal(color, 'red');
+    })
+    this.set('colors', [{ color: 'red'}]);
+    await render(hbs `
+      <Product::Details
+        @price={{this.price}}
+        @colors={{this.colors}}
+        @onChangeColor={{this.onChangeColor}}
+        @isDetails={{true}}
+      />
     `);
+    assert.dom('[data-test-original-price]').hasText('$50.00');
+    assert.dom('[data-test-current-price]').hasText('$30.00');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await click('[data-test-color]')
   });
 });
